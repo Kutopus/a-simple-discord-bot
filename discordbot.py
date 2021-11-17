8c88f0102e12527ca32b70c98e2cf092575fdede
Kuto <86799382+Kutopus@users.noreply.github.com>
2021-11-17 15:41:50 -0300

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,8 +4,8 @@ import discord
 
 client = discord.Client()
 bot = commands.Bot(command_prefix="?")
-adms = [226880541738139648, 543109910506766338, 529801585451335680, 322803186475728896]
-comandos = ["ajuda", "oi", "tchau", "moeda", "tocaaqui", "hf", "linda", "avatar"]
+adms = [a, b, c, d]   # Change the admins here
+comandos = ["help", "hi", "bye", "coin", "highfive", "hf", "beautiful", "avatar"]
 comandos.sort()
 comandos = " | ".join(comandos)
 
@@ -18,41 +18,39 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content == "?oi":
-        await message.channel.send("Oiii")
-    if message.content == "?tchau":
-        await message.channel.send("Tchauzinho")
-    if message.content == "?moeda":
+    if message.content == "?hi":
+        await message.channel.send("hiiii")
+    if message.content == "?bye":
+        await message.channel.send("bye bye")
+    if message.content == "?coin":
         choice = random.randint(1, 2)
         if choice == 1:
-            await message.channel.send("caraaa")
+            await message.channel.send("heads")
         elif choice == 2:
-            await message.channel.send("coroaa")
-    if message.content == "?tocaaqui" or message.content == "?hf":
+            await message.channel.send("tails")
+    if message.content == "?highfive" or message.content == "?hf":
         await message.add_reaction("✋")
-    if message.content == "?oid":
+    if message.content == "?iadmin":
         for adm in adms:
             if message.author.id == adm:
-                await message.channel.send("oi adm linda fofa")
+                await message.channel.send("hi adm love you")
                 break
-    if message.content == "?linda":
-        await message.channel.send("<@226880541738139648>")
     if message.content == "?avatar":
         await message.channel.send(message.author.avatar_url)
-    if message.content == "?ajuda":
+    if message.content == "?help":
         await message.channel.send(f'Comandos: {comandos}')
-    if message.content == "?secreto":
-        await message.channel.send("Não tem secreto bobinho(a)")
-    if message.content == "?geo":
-        await message.channel.send("geolink")
+    if message.content == "?secret":
+        await message.channel.send("there is no secreeet")
 
-@bot.command()
+        
+# Ignore that
+'''@bot.command()
 async def args(ctx, arg1, arg2):
     if ctx.content.startWith("?mensagem"):
-        await ctx.channel.send('You sent {} and {}'.format(arg1, arg2))
+        await ctx.channel.send('You sent {} and {}'.format(arg1, arg2))'''
 
 
 
 
 
-client.run("ODkyMDQwNjUxMjg4NDc3NzQ2.YVHHmA.J_QwExfYM7uKNJ0VSEs2nfXGhX4")
+client.run("") # Token
